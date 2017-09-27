55c065e961bc42fb02cb15655a3df7afcd15f686
Yang Piao <yp@yangpiao.net>
2017-09-27 02:11:29 -0700

    Prototype

---
+++ bin/hl2img.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const path = require('path');
+
+const STYLE = '--style=';
+const OUTPUT = '--output=';
+const OUTPUT_SHORT = '-o';
+const WIDTH = '--width=';
+const DEBUG = '--debug';
+
+const options = {
+  input: process.argv[2]
+};
+
+process.argv.slice(3).forEach((arg, i, args) => {
+  if (arg.startsWith(STYLE)) {
+    options.style = arg.substring(STYLE.length);
+  } else if (arg.startsWith(OUTPUT)) {
+    options.output = arg.substring(OUTPUT.length);
+  } else if (arg === OUTPUT_SHORT) {
+    options.output = args[i + 1];
+  } else if (arg.startsWith(WIDTH)) {
+    options.width = Number(arg.substring(WIDTH.length));
+  } else if (arg === DEBUG) {
+    options.debug = true;
+  }
+});
+
+if (!options.output) {
+  options.output = path.resolve(process.cwd(), 'output.png');
+}
+
+require('../lib/run')(options);
---
+++ readme.md
@@ -0,0 +1,5 @@
+Highlight code and save to an image.
+
+```
+bin/hl2img.js {input} -o {output} [--style={css}] [--width={width}] [--debug]
+```
